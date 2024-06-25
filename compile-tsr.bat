c8e7b51df2229290313b1cc654d0a643ade79f4b
Erik R <erik@erikszeug.de>
2024-06-25 23:39:47 +0200

    Add blank resource files with other nodes from vanilla files
    All files contain a version node, and resources.DLL a bunch more dialogs that may be required by the game under specific circumstances to function.

--- compile-tsr.bat
+++ compile-tsr.bat
@@ -2,10 +2,10 @@
 if not exist "output\EXE" (
   mkdir output\EXE
 )
-"external/frc.exe" resources/equipresources.frc output/EXE/equipresources.dll
-"external/frc.exe" resources/infocards.frc output/EXE/infocards.dll
-"external/frc.exe" resources/misctext.frc output/EXE/misctext.dll
-"external/frc.exe" resources/misctextinfo2.frc output/EXE/misctextinfo2.dll
-"external/frc.exe" resources/nameresources.frc output/EXE/nameresources.dll
-"external/frc.exe" resources/offerbriberesources.frc output/EXE/offerbriberesources.dll
-"external/frc.exe" resources/resources.frc output/EXE/resources.dll
+"external/frc.exe" -c resources/equipresources.frc output/EXE/equipresources.dll
+"external/frc.exe" -c resources/infocards.frc output/EXE/infocards.dll
+"external/frc.exe" -c resources/misctext.frc output/EXE/misctext.dll
+"external/frc.exe" -c resources/misctextinfo2.frc output/EXE/misctextinfo2.dll
+"external/frc.exe" -c resources/nameresources.frc output/EXE/nameresources.dll
+"external/frc.exe" -c resources/offerbriberesources.frc output/EXE/offerbriberesources.dll
+"external/frc.exe" -c resources/resources.frc output/EXE/resources.dll
