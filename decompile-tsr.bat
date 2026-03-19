34860c3ccf5e6509140a4034daa7fd6bdfc8f848
BC46 <cees3331x00@gmail.com>
2026-03-19 21:44:21 +0100

    Convert FRC files to UTF-8
    Turns out all of the characters that are used in the FRC files fit within the ANSI range. Hence the files were converted to UTF-8 without causing problems. Now GitHub should correctly track the changes in every commit. Should've done this ages ago...

--- decompile-tsr.bat
+++ decompile-tsr.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
-"external/res2frc.exe" -u -w 0 output/EXE/equipresources.dll -o resources/equipresources.frc
-"external/res2frc.exe" -u -w 0 output/EXE/infocards.dll -o resources/infocards.frc
-"external/res2frc.exe" -u -w 0 output/EXE/misctext.dll -o resources/misctext.frc
-"external/res2frc.exe" -u -w 0 output/EXE/misctextinfo2.dll -o resources/misctextinfo2.frc
-"external/res2frc.exe" -u -w 0 output/EXE/nameresources.dll -o resources/nameresources.frc
-"external/res2frc.exe" -u -w 0 output/EXE/offerbriberesources.dll -o resources/offerbriberesources.frc
-"external/res2frc.exe" -u -w 0 output/EXE/resources.dll -o resources/resources.frc
-"external/res2frc.exe" -u -w 0 output/EXE/serverresources.dll -o resources/serverresources.frc
+"external/res2frc.exe" -w 0 output/EXE/equipresources.dll -o resources/equipresources.frc
+"external/res2frc.exe" -w 0 output/EXE/infocards.dll -o resources/infocards.frc
+"external/res2frc.exe" -w 0 output/EXE/misctext.dll -o resources/misctext.frc
+"external/res2frc.exe" -w 0 output/EXE/misctextinfo2.dll -o resources/misctextinfo2.frc
+"external/res2frc.exe" -w 0 output/EXE/nameresources.dll -o resources/nameresources.frc
+"external/res2frc.exe" -w 0 output/EXE/offerbriberesources.dll -o resources/offerbriberesources.frc
+"external/res2frc.exe" -w 0 output/EXE/resources.dll -o resources/resources.frc
+"external/res2frc.exe" -w 0 output/EXE/serverresources.dll -o resources/serverresources.frc
